ad6f2e20252dc462fa9352fa56e141c4ae348dd6
Eli <elijames2301@gmail.com>
2023-10-15 23:32:22 -0400

    write happy number solution

---
+++ happynumber.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        past=set()
+        while n !=1:
+            n=sum(int(i)**2 for i in str(n))
+            if n in past:
+                return False
+            past.add(n)
+
+        return True
+        
